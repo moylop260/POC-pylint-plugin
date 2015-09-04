bd9a42a22dc3630f329dd273415e4c70a737258e
Moises Lopez <moylop260@vauxoo.com>
2015-09-04 18:01:20 +0000

    [REF] using pbr

--- pylint_oca/augmentations/main.py
+++ pylint_oca/augmentations/main.py
@@ -7,6 +7,8 @@ from .. import settings
 
 
 def is_manifest_file(node):
+    """Verify if the node file is a manifest file
+    :return: Boolean `True` if is manifest file else `False`"""
     filename = os.path.basename(node.root().file)
     is_manifest = filename in settings.MANIFEST_FILES
     return is_manifest
--- setup.py
+++ setup.py
@@ -5,14 +5,7 @@ import setuptools
 from pip.req import parse_requirements
 from pip.download import PipSession
 
-file_requirements = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "requirements.txt")
-
-install_reqs = parse_requirements(file_requirements, session=PipSession)
-reqs = [str(ir.req) for ir in install_reqs]
 
 setuptools.setup(setup_requires=['pbr'],
                  pbr=True,
-                 install_requires=reqs,
                  package_data={'': ['*.yaml']})
