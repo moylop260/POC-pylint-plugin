5e07e9259cc226f379adb1229d354b31e3916d82
Moises Lopez <moylop260@vauxoo.com>
2015-09-04 04:37:31 +0000

    [REM] Removing base chck

--- pylint_oca/__init__.py
+++ pylint_oca/__init__.py
@@ -7,15 +7,4 @@ def register(linter):
     linter.register_checker(checkers.modules_odoo.ModuleChecker(linter))
     linter.register_checker(checkers.no_modules.NoModuleChecker(linter))
     linter.register_checker(checkers.format.FormatChecker(linter))
-    linter.register_checker(checkers.base.CustomBasicChecker(linter))
 
-all_msgs = {}
-all_msgs.update(checkers.modules_odoo.OCA_MSGS)
-all_msgs.update(checkers.no_modules.OCA_MSGS)
-all_msgs.update(checkers.format.OCA_MSGS)
-all_msgs.update(checkers.base.OCA_MSGS)
-print ',\n    '.join(sorted([name_key for msg_code, (title, name_key, description) in all_msgs.iteritems()]))
-for msg_code, (title, name_key, description) in \
-                sorted(all_msgs.iteritems()):
-    print "\t- {0} - {1} - {2}".format(msg_code, title, name_key)
-print ','.join(sorted(all_msgs.keys()))
