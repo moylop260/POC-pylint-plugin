3be15de9aac0e8467cb12ee389f827af72948712
Moises Lopez <moylop260@vauxoo.com>
2015-09-08 08:44:41 -0500

    mv tests->pylint_oca/test

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ import setuptools
 
 setuptools.setup(setup_requires=['pbr'],
                  pbr=True,
-                 test_suite="tests",
+                 test_suite="pylint_oca.test",
                  package_data={'': ['*.yaml']})
